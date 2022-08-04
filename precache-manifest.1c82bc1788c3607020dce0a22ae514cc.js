8228e33cd23a88d4b5b1a8cc21faa99aa84144f4
ariadnabonive <ariadnabonive@gmail.com>
2022-08-04 15:41:49 -0300

    Updates

---
+++ precache-manifest.1c82bc1788c3607020dce0a22ae514cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5eb43a5c3871e5cc07d8",
+    "url": "/To-Do-React/static/css/main.37210a66.chunk.css"
+  },
+  {
+    "revision": "5eb43a5c3871e5cc07d8",
+    "url": "/To-Do-React/static/js/main.5eb43a5c.chunk.js"
+  },
+  {
+    "revision": "4e7011529b9545b6cd26",
+    "url": "/To-Do-React/static/js/1.4e701152.chunk.js"
+  },
+  {
+    "revision": "1731dd4c4f4598d1499a",
+    "url": "/To-Do-React/static/js/runtime~main.1731dd4c.js"
+  },
+  {
+    "revision": "b3cf7db3d1e0b9b8e5bf15705aeb35d6",
+    "url": "/To-Do-React/index.html"
+  }
+];
