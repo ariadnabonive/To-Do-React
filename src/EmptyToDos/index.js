5aa5cdd1126925fc406df0b56be2450a04f7e1ab
ariadnabonive <ariadnabonive@gmail.com>
2022-08-05 11:08:25 -0300

    Se cambian algunos textos y se agraga una imagen a empty todo

--- src/EmptyToDos/index.js
+++ src/EmptyToDos/index.js
@@ -2,7 +2,16 @@ import React from 'react';
 import './EmptyToDos.css';
 
 function EmptyToDos () {
-    return <p className='EmptyToDos-text'>Agrega un nuevo To Do a tu lista.</p>;
-}
+    return (
+        <div>
+            <p className='EmptyToDos-text'>No se encontró ningún TO DO.</p>
+            
+            <img className='EmptyToDos-img' src="https://media.istockphoto.com/vectors/discussion-vector-id1185520362?k=20&m=1185520362&s=612x612&w=0&h=T-qpxN-SdshRHPXTOQn35t1t62c5RZi-mcwk4qo8kGY=" alt=''></img>
+        
+        </div>
+        
+
+    );
+};
 
 export { EmptyToDos };
